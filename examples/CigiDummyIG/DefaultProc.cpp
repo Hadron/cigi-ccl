0e0a2e08c90707792011de7273f2436ceda988ac
gregbasler <gregbasler@7298b0d4-bb18-0410-8e19-c8aa18815fbc>
2009-03-02 23:00:14 +0000

    Documentation correction
    git-svn-id: http://cigi.svn.sourceforge.net/svnroot/cigi/trunk/ccl@504 7298b0d4-bb18-0410-8e19-c8aa18815fbc

--- examples/CigiDummyIG/DefaultProc.cpp
+++ examples/CigiDummyIG/DefaultProc.cpp
@@ -1,22 +1,21 @@
 /** <pre>
- *  This Class will process the "Animation Stop Event Notification"
- *   for the LCPS
- *  
+ *  This Class will process any unassigned packets
+ *
  *  FILENAME:   DefaultProc.cpp
  *  LANGUAGE:   C++
  *  CLASS:      UNCLASSIFIED
- *  PROJECT:    LCPS
- *  
- *  PROGRAM DESCRIPTION: 
+ *  PROJECT:    CIGI
+ *
+ *  PROGRAM DESCRIPTION:
  *  ...
- *  
+ *
  *  MODIFICATION NOTES:
  *  DATE     NAME                                SCR NUMBER
  *  DESCRIPTION OF CHANGE........................
- *  
+ *
  *  04/29/2005 Greg Basler                       INIT
  *  Initial Release.
- *  
+ *
  * </pre>
  *  Author: The Boeing Company
  *  Version: 0.1
--- examples/CigiDummyIG/DefaultProc.h
+++ examples/CigiDummyIG/DefaultProc.h
@@ -1,22 +1,21 @@
 /** <pre>
- *  This Class will process the "Animation Stop Event Notification"
- *   for the LCPS
- *  
+ *  This Class will process any unassigned packets
+ *
  *  FILENAME:   DefaultProc.h
  *  LANGUAGE:   C++
  *  CLASS:      UNCLASSIFIED
- *  PROJECT:    LCPS
- *  
- *  PROGRAM DESCRIPTION: 
+ *  PROJECT:    CIGI
+ *
+ *  PROGRAM DESCRIPTION:
  *  ...
- *  
+ *
  *  MODIFICATION NOTES:
  *  DATE     NAME                                SCR NUMBER
  *  DESCRIPTION OF CHANGE........................
- *  
+ *
  *  04/29/2005 Greg Basler                       INIT
  *  Initial Release.
- *  
+ *
  * </pre>
  *  Author: The Boeing Company
  *  Version: 0.1
@@ -55,8 +54,8 @@ public:
    //==> Incoming Packet Processing
 
    //=========================================================
-   //! The callback handler for the CIGI SOF packet
-   //! \param Packet - The pointer to the SOF packet object.
+   //! The default callback handler for the CIGI
+   //! \param Packet - The pointer to the default packet object.
    //!
    virtual void OnPacketReceived(CigiBasePacket *Packet);
 
